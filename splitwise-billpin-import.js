0db0eef556ebadb88f0531fa9414cf8f0eea4ffa
Jeff Gabhart <lazydesert@gmail.com>
2014-10-06 10:40:51 -0500

    Post the natural language sentence to the parse_sentence api.

--- splitwise-billpin-import.js
+++ splitwise-billpin-import.js
@@ -2,6 +2,7 @@ var _ = require('lodash');
 var OAuth = require('oauth');
 var optimist = require('optimist');
 var prompt = require('prompt');
+var util = require('util');
 
 var schema = {
   properties: {
@@ -47,5 +48,18 @@ prompt.get(schema, function(err, input) {
     console.log('Date: ' + tx.happenedAt.iso.substring(0, tx.happenedAt.iso.indexOf('T')));
     var natural = tx.receiver.name + ' owes ' + tx.sender.name + ' $' + tx.amount + ' for ' + tx.message + ' on ' + tx.happenedAt.iso.substring(0, tx.happenedAt.iso.indexOf('T'));
     console.log(natural);
+    oauth.post(
+      'https://secure.splitwise.com/api/v3.0/parse_sentence',
+      input.accessToken,
+      input.accessSecret,
+      {
+        input: natural,
+        friend_id: input.friendId
+      },
+      null,
+      function(err, data, response) {
+        console.log(util.inspect(data));
+      }
+    );
   });
 });
